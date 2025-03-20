8bd03671c4246d565627556cd6eae93f0e7a094f
Tavis <github@tavis.la>
2025-03-19 23:44:44 -1000

    fix for single frame delay in looped draw_webp

--- src/gfx.c
+++ src/gfx.c
@@ -34,18 +34,35 @@ int gfx_initialize(const void *webp, size_t len) {
     ESP_LOGE(TAG, "Already initialized");
     return 1;
   }
+  // ESP_LOGI(TAG, String(esp_get_free_heap_size()));
+  int heap = esp_get_free_heap_size();
+  int heapl = heap_caps_get_largest_free_block(MALLOC_CAP_DEFAULT);
 
+  ESP_LOGE(TAG, "largest heap %d", heapl);
+  // ESP_LOGI(TAG, "calling calloc");
   // Initialize state
+  ESP_LOGI(TAG, "Allocating buffer of size: %d", len);
+  int sizeo = sizeof(struct gfx_state);
+  ESP_LOGI(TAG, "Allocating size of  of size: %d", sizeof(struct gfx_state));
+
   _state = calloc(1, sizeof(struct gfx_state));
   _state->len = len;
+  ESP_LOGI(TAG, "calloc buff");
   _state->buf = calloc(1, len);
+  ESP_LOGI(TAG, "done calloc, copying");
+  if (_state->buf == NULL) {
+    ESP_LOGE("gfx", "Memory allocation failed!");
+    return 1;
+  }
   memcpy(_state->buf, webp, len);
+  ESP_LOGI(TAG, "done, copying");
 
   _state->mutex = xSemaphoreCreateMutex();
   if (_state->mutex == NULL) {
     ESP_LOGE(TAG, "Could not create gfx mutex");
     return 1;
   }
+  ESP_LOGI(TAG, "done with gfx init");
 
   // Initialize the display
   if (display_initialize()) {
@@ -53,14 +70,15 @@ int gfx_initialize(const void *webp, size_t len) {
   }
 
   // Launch the graphics loop in separate task
-  BaseType_t ret = xTaskCreatePinnedToCore(gfx_loop,             // pvTaskCode
-                                           "gfx_loop",           // pcName
-                                           GFX_TASK_STACK_SIZE,  // usStackDepth
-                                           (void*)&isAnimating,  // pvParameters
-                                           GFX_TASK_PRIO,        // uxPriority
-                                           &_state->task,        // pxCreatedTask
-                                           GFX_TASK_CORE         // xCoreID
-  );
+  BaseType_t ret =
+      xTaskCreatePinnedToCore(gfx_loop,              // pvTaskCode
+                              "gfx_loop",            // pcName
+                              GFX_TASK_STACK_SIZE,   // usStackDepth
+                              (void *)&isAnimating,  // pvParameters
+                              GFX_TASK_PRIO,         // uxPriority
+                              &_state->task,         // pxCreatedTask
+                              GFX_TASK_CORE          // xCoreID
+      );
   if (ret != pdPASS) {
     ESP_LOGE(TAG, "Could not create gfx task");
     return 1;
@@ -153,23 +171,27 @@ static void gfx_loop(void *args) {
       *isAnimating = 0;
     }
     // vTaskDelay(pdMS_TO_TICKS(500)); // delay for anti barf
-
   }
 }
 
 static int draw_webp(uint8_t *buf, size_t len, int32_t *isAnimating) {
   // Set up WebP decoder
+  ESP_LOGI(TAG, "starting draw_webp");
   int app_dwell_secs = *isAnimating;
 
-  int64_t start_us = esp_timer_get_time();
-
   int64_t dwell_us;
-  if (app_dwell_secs == 0 ) {
-    ESP_LOGW(TAG,"isAnimating is already 0. Looping one more time while we wait.");
-    dwell_us = 1 * 1000000; // default to 1s if it's zero so we loop again or show the image for 1 more second.
+  int64_t dwell_ms;
+
+  if (app_dwell_secs == 0) {
+    ESP_LOGW(TAG,
+             "isAnimating is already 0. Looping one more time while we wait.");
+    dwell_us = 1 * 1000000;  // default to 1s if it's zero so we loop again or
+                             // show the image for 1 more second.
+    dwell_ms = 1 * 1000;
   } else {
     // ESP_LOGI(TAG, "dwell_secs : %d", app_dwell_secs);
     dwell_us = app_dwell_secs * 1000000;
+    dwell_ms = app_dwell_secs * 1000;
   }
   // ESP_LOGI(TAG, "frame count: %d", animation.frame_count);
 
@@ -193,7 +215,7 @@ static int draw_webp(uint8_t *buf, size_t len, int32_t *isAnimating) {
     ESP_LOGE(TAG, "Could not get WebP animation");
     return 1;
   }
-
+  int64_t start_us = esp_timer_get_time();
   while (esp_timer_get_time() - start_us < dwell_us) {
     int lastTimestamp = 0;
     int delay = 0;
@@ -206,9 +228,12 @@ static int draw_webp(uint8_t *buf, size_t len, int32_t *isAnimating) {
       WebPAnimDecoderGetNext(decoder, &pix, &timestamp);
       if (delay > 0) {
         xTaskDelayUntil(&drawStartTick, pdMS_TO_TICKS(delay));
+      } else {
+        vTaskDelay(10);  // small delay for yield.
       }
       drawStartTick = xTaskGetTickCount();
-      display_draw(pix, animation.canvas_width, animation.canvas_height, 4, 0, 1, 2);
+      display_draw(pix, animation.canvas_width, animation.canvas_height, 4, 0,
+                   1, 2);
       delay = timestamp - lastTimestamp;
       lastTimestamp = timestamp;
     }
@@ -219,20 +244,25 @@ static int draw_webp(uint8_t *buf, size_t len, int32_t *isAnimating) {
     if (delay > 0) {
       xTaskDelayUntil(&drawStartTick, pdMS_TO_TICKS(delay));
     } else {
-      vTaskDelay(pdMS_TO_TICKS(10));  // Add a small fallback delay to yield CPU
+      vTaskDelay(
+          pdMS_TO_TICKS(100));  // Add a small fallback delay to yield CPU
     }
 
     // In case of a single frame, sleep for app_dwell_secs
     if (animation.frame_count == 1) {
-      ESP_LOGI(TAG, "single frame delay");
-      xTaskDelayUntil(&drawStartTick, pdMS_TO_TICKS(app_dwell_secs * 1000));
+      ESP_LOGI(TAG, "single frame delay for %d", app_dwell_secs);
+      // xTaskDelayUntil(&start_us, dwell_us);
+      vTaskDelay(pdMS_TO_TICKS(dwell_ms));  // full dwell delay
+      // *isAnimating = 0;
       break;
     }
   }
   WebPAnimDecoderDelete(decoder);
   if (app_dwell_secs != 0) {
     ESP_LOGI(TAG, "Setting isAnimating to 0");
-    *isAnimating = 0;  // only set this to zero if it wasn't already set to zero. setting it again might overwrite what the main loop did while we were re-looping
+    *isAnimating = 0;  // only set this to zero if it wasn't already set to
+                       // zero. setting it again might overwrite what the main
+                       // loop did while we were re-looping
   }
   return 0;
 }
