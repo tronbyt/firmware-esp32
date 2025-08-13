247bcba5207ef3f88b5fe4c45b5d8e701ef94a04
Tavis <github@tavis.la>
2025-08-13 11:04:40 -1000

    add windytron specifics

--- lib/assets/assets.h
+++ lib/assets/assets.h
@@ -6,8 +6,8 @@
 extern const uint8_t ASSET_BOOT_WEBP[];
 extern const size_t ASSET_BOOT_WEBP_LEN;
 
-#ifdef BOOT_WEBP_PARROT
-#include "lib/assets/parrot_c"
+#ifdef BOOT_WEBP_WINDYTRON
+#include "lib/assets/windytron_c"
 #else
 #include "lib/assets/tronbyt_c"
 #endif
